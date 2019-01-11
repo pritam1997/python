0561b35d954b11db34ab72548884c136cca8c256
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-11 13:35:02 +0530

    Add files via upload
    11-01-19

---
+++ calculator.py
@@ -0,0 +1,18 @@
+var=int(input("Enter the first value :"))
+var2=int(input("Enter the second value :"))
+
+add=var+var2
+sub=var-var2
+mult=var*var2
+div=var/var2
+remd=var%var2
+exp=var**var2
+quo=var//var2
+
+print("Addition of two no. is: ", add)
+print("Subtraction of two no. is: ", sub)
+print("Multiplication of two no. is: ", mult)
+print("Division of two no. is: ", div)
+print("AReminder of two no. is: ", remd)
+print("Quotient of two no. is: ", quo)
+print("Exponential of two no. is: ", exp)
