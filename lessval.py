90be37f21d4de6df4c34324bdc521ce4e9887ccf
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-11 14:18:15 +0530

    Add files via upload

---
+++ lessval.py
@@ -0,0 +1,26 @@
+a = 10
+b = 20
+c = 30
+d = 14
+e = 2
+f = 3
+g = 4
+
+if a<b:
+	print("a is smallest no.")
+elif a<c:
+	print("a is smallest no.")
+elif a<
+elif b<3:
+	print("b is smallest no.")
+elif c<3:
+	print("c is smallest no.")
+elif d<3:
+	print("d is smallest no.")
+elif e<3:
+	print("e is smallest no.")
+elif f<3:
+	print("f is smallest no.")
+elif g<3:
+	print("g is smallest no.")
+	
