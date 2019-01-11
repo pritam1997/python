286719d8a9891d4b42cb768c65c26639cec1c315
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-11 21:05:48 +0530

    Add files via upload
    11-1-19

--- lessval.py
+++ lessval.py
@@ -7,20 +7,59 @@ f = 3
 g = 4
 
 if a<b:
-	print("a is smallest no.")
-elif a<c:
-	print("a is smallest no.")
-elif a<
-elif b<3:
+	if a<c:
+		if a<d:
+			if a<e:
+				if a<f:
+					if a<g:
+						print("a is smallest no.")
+					elif g<b:
+						print("g is smallest no.")
+					elif g<c:
+						print("g is smallest no.")
+					elif g<d:
+						print("g is smallest no.")
+					elif g<e:
+						print("g is smallest no.")
+					elif g<f:
+						print("g is smallest no.")
+				elif f<b:
+					print("f is smallest no.")
+				elif f<c:
+					print("f is smallest no.")
+				elif f<d:
+					print("f is smallest no.")
+				elif f<e:
+					print("f is smallest no.")
+				elif f<g:
+					print("f is smallest no.")
+			elif e<b:
+					print("e is smallest no.")
+			elif e<c:
+					print("e is smallest no.")
+			elif e<d:
+					print("e is smallest no.")
+			elif e<f:
+					print("e is smallest no.")
+			elif e<g:
+					print("e is smallest no.")
+		elif d<b:
+			print("d is smallest no.")
+		elif d<c:
+			print("d is smallest no.")
+		elif d<e:
+			print("d is smallest no.")
+		elif d<f:
+			print("d is smallest no.")
+		elif d<g:
+			print("d is smallest no.")
+	elif c<b:
+		print("c is smallest no.")
+	elif c<d:
+		print("c is smallest no.")
+	elif c<e:
+		print("c is smallest no.")
+	elif c<f:
+		print("c is smallest no.")
+else:
 	print("b is smallest no.")
-elif c<3:
-	print("c is smallest no.")
-elif d<3:
-	print("d is smallest no.")
-elif e<3:
-	print("e is smallest no.")
-elif f<3:
-	print("f is smallest no.")
-elif g<3:
-	print("g is smallest no.")
-	
