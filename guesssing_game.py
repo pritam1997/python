4dabbdc98761390c1d6ece3952c8b49002336a24
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-15 19:18:41 +0530

    Add files via upload
    12-1-19

--- guesssing_game.py
+++ guesssing_game.py
@@ -1,4 +1,11 @@
 import random
-
+print("guessing game between 1 to 10 number")
+n = int(input("Enter no. :"))
 r = random.randint(1,10)
+if n == r: 
+	print(f"Your no. {n} is matched")
+elif n > 10:
+	print("Your no. is greater than 10")
+else:
+	print("Fail!!!.. Try again..")
 print(r)
