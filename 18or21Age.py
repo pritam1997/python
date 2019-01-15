c39e723ad8fffa52b8ffb591e8a8aa8e183cee0e
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-15 19:23:52 +0530

    Update 18or21Age.py

--- 18or21Age.py
+++ 18or21Age.py
@@ -1,7 +1,7 @@
 age = int(input("Enter your age to valid for entry :"))
-if age>=18 and age<=21:
-	print("Take Wrist Band you are able to enter in party \nBut not permission to drink alcohol")
-elif age>21:
-	print("Full Permission")
+if age >= 18 and age <= 21:
+	print("Take Wrist Band you are able to enter in party.\nBut not permission to drink alcohol.")
+elif age > 21:
+	print("You can visit to the party.")
 else:
-	print("Get Lost")
+	print("You are too young.\nThank you for coming.")
