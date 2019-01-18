d62f4fd2a591157cd8941ae34d3ae440cba2a1d3
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-18 12:06:43 +0530

    Add files via upload
    18-1-19

--- guesssing_game.py
+++ guesssing_game.py
@@ -2,10 +2,13 @@ import random
 print("guessing game between 1 to 10 number")
 n = int(input("Enter no. :"))
 r = random.randint(1,10)
-if n == r: 
-	print(f"Your no. {n} is matched")
-elif n > 10:
-	print("Your no. is greater than 10")
+if n < 10:
+	if n == r: 
+		print(f"Your no. {n} is matched")
+	elif n > r:
+		print("Your no. is too large")
+	else:
+		print("your no. is too small")
 else:
-	print("Fail!!!.. Try again..")
+	print("Invalid input")
 print(r)
