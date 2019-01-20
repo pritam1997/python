7dd0b08bcb9ce4eb06285ca3ad4dddbcfe81bc81
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-20 15:23:42 +0530

    Add files via upload
    20-1-19

--- reverse_name.py
+++ reverse_name.py
@@ -1,1 +1,15 @@
-s = str(input("Enter your name :"))
+s = str(input("Enter your name to reverse : "))
+
+i=len(s)-1
+while i >= 0:
+	print(f"{s[i]}")
+	i -= 1
+
+# n=0
+# count=1
+# while n>-1:
+# 	if count==n:
+# 		break
+# 	print(f"{s[n]}")
+# 	count+=1
+# 	n += 1
