32b770c2d721a4e970b87ac7961b18c417186a85
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-23 13:37:20 +0530

    Add files via upload
    23-13-19

---
+++ loop_1.py
@@ -0,0 +1,5 @@
+for a in range(1,2):
+	for b in range(1,4):
+		for c in range(3,4):
+			if a%2==0 or c%2==0:
+				print(f"{a}{b}{c}")
---
+++ loops.py
@@ -0,0 +1,4 @@
+s = str(input("Enter string : "))
+n = int(input("How many times you want to scream :"))
+for x in range(1,n+1):
+	print(f"{s}")
