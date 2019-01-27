336f43764bd284aa306fa004fc24dc2eb0c17405
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-01-27 20:14:13 +0530

    Add files via upload
    27-1-19

--- list_no.py
+++ list_no.py
@@ -1,4 +1,9 @@
-n = int(input("Enter no. : "))
-l = [1:n]
-for x in l:
-	print(f"{x*10}")
+n = int(input("Enter odd no. : "))
+# like no. is 5
+# printing 5 times and decrementing by 1 in loop (5-1)
+l = []
+for x in range(1,n+1):
+	l.append(x)
+
+for i in range(n):
+	print(l[i])
