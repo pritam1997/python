f8a5ae552ffb840c27fd5d6a7889cda446dbd281
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-02-19 19:49:40 +0530

    Add files via upload

--- count_str.py
+++ count_str.py
@@ -1,29 +1,6 @@
-# s=input("Enter string : ")
-s="asdasdq"
+s=input("Enter string : ")
 def fun(s):
-	l=len(s)
-	c=1
-	
-	for x in range(l):
-		if x==0:
-			a=1
-		elif x==1:
-			a=2
-		elif x==2:
-			a=3
-		elif x==3:
-			a=4
-		elif x==4:
-			a=5
-		for i in range(a,l):
-			if x==i:
-				break		
-			if s[x]==s[i]:
-				c+=1
-				t=c
-
-	print(t)
-	# a=input("which letter you want to count : ")
-	# c=s.count(a)
-	# return c
+	l=list(s)
+	for i in s:
+		print(f"{i} = {l.count(i)}")
 fun(s)
