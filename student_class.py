79f1191297386fc42974e5b362701df7dbb3a6e2
pritam1997 <46590926+pritam1997@users.noreply.github.com>
2019-03-07 00:35:55 +0530

    Add files via upload
    05-01-19

--- student_class.py
+++ student_class.py
@@ -1,33 +1,52 @@
-class stud:
-	def __init__(s,firstname,lastname,rollno):
-		s.firstname=firstname
-		s.lastname=lastname
-		s.rollno=rollno
-
-	def student_details(s):
-		return f"{s.firstname} {s.lastname}\t{s.rollno}"
-
-	def stud_age(age):
-		if age<5 or age>16:
-			print("You are not eligible")
-		elif age>16:
-			print("Are you has backlog")
-
-		
-
-	def stud_dob(dob):
-
-	def sports(s):
-		return f"{s.sport}"
-
-	def exam(s):
-		return f"{s.percent}" 
+class student:
+	def __init__(self,name,rollno):
+		self.name=name
+		self.rollno=rollno
 	
-sujit=stud("Sujit","singh",12,5,22,'90%','Football')
+	
+	def student_details(self):
+		return f"{self.name.capitalize()},{self.rollno}"
 
-deepak=stud("Deepak","nagre",45,'15th',23,'80%','Cricket')
+	def standard_marks(self,std,marks):
+		if std<1 or std>10:
+			print("Standard upto 1 to 10")
+		else:
+			c=str(std)
+			if c[-1]=='1':
+				e='st'
+			elif c[-1]=='2':
+				e='nd'
+			elif c[-1]=='3':
+				e='rd'
+			else:
+				e='th'
+			return f"{self.name.capitalize()} got {marks}% in {std}{e} Standard"
 
-print("Student name\tRoll no.\tStandard\tBirthday\tPercentage\tSports")
+	def age(self,ag):
+		return f"{self.name.capitalize()} is {ag} year old"
 
-print(sujit.student_details(),"\t\t",sujit.exam(),"\t\t",sujit.sports())
-print(deepak.student_details(),"\t\t",sujit.exam(),"\t\t",sujit.sports())
+	def dob(self,date,mon,yr):
+		if yr <1998 or yr >2014:
+			print("Only 1998 to 2014 birth student are allow")
+		else:
+			n=2019-yr
+			print(self.age(n))	
+		return f"{self.name.capitalize()} birthday on {date}-{mon}-{yr}"
+
+	def sports(self,sp):
+		return f"{self.name.capitalize()} is interested in {sp}"
+
+s=student("sujit",12)
+a=s.standard_marks(9,80)
+print(a)
+a=s.dob(10,12,1999)
+print(a)
+a=s.sports("football")
+
+s1=student("rohit",34)
+a=s1.standard_marks(3,90)
+print(a)
+a=s1.dob(1,2,2015)
+print(a)
+a=s1.sports("chess")
+
